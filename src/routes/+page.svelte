26f75f5781c9a6fc1bda263de8fc8ed2ce576070
Username-ray <maedara96@craighead.school.nz>
2024-06-11 14:12:31 +1200

    Added Button connected to a function

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script>
   import Header from "$lib/Header.svelte"
   import ImageCard from "../lib/ImageCard.svelte"
+  let count = 0
+  function increment() {
+    count = count + 1
+  }
+  function decrement() {
+    count = count - 1
+  }
 </script>
 
 <Header />
@@ -9,6 +16,10 @@
   <h2>SvelteKit</h2>
 
   <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
+
+  <p>Count: {count}</p>
+  <button on:click={increment}>Increment</button>
+  <button on:click={decrement}>Decrement</button>
 </main>
 
 <ImageCard />
