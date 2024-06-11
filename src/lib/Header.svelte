fb743735dd10a8126fdd587b4760778e937d6988
Username-ray <maedara96@craighead.school.nz>
2024-06-11 14:06:27 +1200

    Added Input Element with Binding

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,6 +2,8 @@
   let name = "world"
 </script>
 
+<input bind:value={name} placeholder="Enter your name" />
+
 <header class="hero">
   <div class="hero-body">
     <h1 class="title">Hello {name}!</h1>
