33e1c0b283c660e0c52089edbff8246dedac44a9
Username-ray <maedara96@craighead.school.nz>
2024-06-11 13:34:20 +1200

    inserted "name" variable value into HTML using {}
    to substitute "world" into the value in the h1 component

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,10 @@
+<script>
+  let name = "world"
+</script>
+
 <header class="hero">
   <div class="hero-body">
-    <h1 class="title">Welcome</h1>
+    <h1 class="title">Hello {name}!</h1>
     <h2 class="subtitle">Welcome to my site</h2>
   </div>
 </header>
