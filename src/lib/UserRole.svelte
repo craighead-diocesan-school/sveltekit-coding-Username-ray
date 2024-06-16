be954c5d61d390c7ce7ea4d2406079509aaefe18
Username-ray <maedara96@craighead.school.nz>
2024-06-17 10:29:26 +1200

    Practice: More User Roles

--- src/lib/UserRole.svelte
+++ src/lib/UserRole.svelte
@@ -3,11 +3,14 @@
 </script>
 
 <main>
+  <h1>Welcome, {role}!</h1>
   {#if role == "Admin"}
-    <h1>Welcome, {role}!</h1>
     <h2>You have full access to the system.</h2>
+  {:else if role == "Editor"}
+    <h2>You can edit content but have limited access.</h2>
+  {:else if role == "Guest"}
+    <h2>You have view-only access.</h2>
   {:else}
-    <h1>Welcome, {role}!</h1>
     <h2>Your acess is limited.</h2>
   {/if}
 </main>
